9df74166c737c193ce7a6e898aedb1526438e0ec
Mandatory Coffee <mandatorycoffee@gmail.com>
2021-05-02 17:09:50 -0400

    Added Bail function for error handling
    - Fixed some incorrect error messages.
    - Removed old script name.

--- rockify.sh
+++ rockify.sh
@@ -5,6 +5,7 @@
 # Bail if not root
 if [[ $EUID -ne 0 ]]; then echo "You must be root."; exit 1; fi
 
+echo -e "\nThis script converts CentOS 8 to Rocky Linux 8."
 echo "This is dangerous! This script may break your OS."
 read -p "Are you sure you wish to continue? (y,n): "
 if [ "$REPLY" != "y" ]; then
@@ -12,7 +13,8 @@ if [ "$REPLY" != "y" ]; then
    exit
 fi
 
-ERR="/tmp/centos2rocky.err"
+
+ERR="/tmp/rockify.err"
 REPO_URL="https://dl.rockylinux.org/pub/rocky/8/BaseOS/x86_64/os/Packages/"
 
 echo -n "Getting current Rocky version... "
@@ -23,29 +25,38 @@ if [ "${#ROCKY_RPMS[@]}" -ne 3 ]; then
   exit 1
 fi
 
+# Figure out full paths of downloaded RPMs.
 ROCKY_RPMS_TMP=`printf "/tmp/%s " ${ROCKY_RPMS[@]}`
 echo "Done."
 
+# Figure out URLs of the Rocky RPMs.
 ROCKY_URLS=`printf "${REPO_URL}%s " ${ROCKY_RPMS[@]}`
 
+# Handle errors
+Bail () {
+  echo -e "\n\nError: $1"
+  echo "Look at $ERR for more info..."
+  exit 1
+}
+
 echo -n "Installing wget, if needed... "
-which wget &> /dev/null || dnf install wget -y 2> $ERR 1> /dev/null || { echo "Couldn't install wget." ; exit 1; }
+which wget &> /dev/null || dnf install wget -y 2> $ERR 1> /dev/null || Bail "Couldn't install wget."
 echo "Done."
 
 echo -n "Downloading Rocky RPMs... "
-wget -P /tmp ${ROCKY_URLS} 2> $ERR 1> /dev/null || { echo 'Failed to download Rocky RPMs.' ; exit 1; }
+wget -P /tmp ${ROCKY_URLS} 2> $ERR 1> /dev/null || Bail "Failed to download Rocky RPMs."
 echo "Done."
 
 echo -n "Clearing dnf cache... "
-rm -rf /var/cache/{yum,dnf} 2> $ERR 1> /dev/null || { echo 'Failed to delete dnf cache.' ; exit 1; }
+rm -rf /var/cache/{yum,dnf} 2> $ERR 1> /dev/null || Bail "Failed to delete dnf cache."
 echo "Done."
 
 echo -n "Removing CentOS RPMs... "
-rpm -e --nodeps centos-linux-repos centos-gpg-keys centos-linux-release 2> $ERR 1> /dev/null || { echo 'Failed to download Rocky RPMs.' ; exit 1; }
+rpm -e --nodeps centos-linux-repos centos-gpg-keys centos-linux-release 2> $ERR 1> /dev/null || Bail "Failed to remove CentOS RPMs."
 echo "Done."
 
 echo -n "Installing Rocky RPMs... "
-rpm -ihv ${ROCKY_RPMS_TMP} 2> $ERR 1> /dev/null || { echo 'Failed to install Rocky RPMs.' ; exit 1; }
+rpm -ihv ${ROCKY_RPMS_TMP} 2> $ERR 1> /dev/null || Bail "Failed to install Rocky RPMs."
 echo "Done."
 
 echo "Syncing distro... "
